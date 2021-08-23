8378c551daf43df9a3adb030c87ebfc7087cfead
Azdine <azdine.eljattari@hotmail.com>
2021-08-23 22:18:53 +0200

    Changes inside app.js

--- EJSCOUCHDB/server/app.js
+++ EJSCOUCHDB/server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const PORT = process.env.PORT || 8000;
 const router = require("../routes/router");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors')
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
